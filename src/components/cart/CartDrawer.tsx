ebdc786cbd42206fc9172a827a8a492f775b6646
Krzysztof <krzysztof.koczy99@gmail.com>
2026-01-07 18:59:45 +0100

    add drawer

---
+++ src/components/cart/CartDrawer.tsx
@@ -0,0 +1,64 @@
+import { useCart } from "../../context/CartContext";
+
+type CartDrawerProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function CartDrawer({ isOpen, onClose }: CartDrawerProps) {
+  const { items, removeFromCart } = useCart();
+
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className="cart-drawer-overlay"></div>
+      
+      <div className="cart-drawer">
+        <div className="cart-drawer-header">
+          <h2>Koszyk</h2>
+          <button className="cart-drawer-close" onClick={onClose}>✕</button>
+        </div>
+        
+        <div className="cart-drawer-content">
+          {items.length === 0 ? (
+            <p>Twój koszyk jest pusty</p>
+          ) : (
+            <ul className="cart-items-list">
+                {items.map((item) => (
+                    <li key={item.product.id}>
+                        <span>{item.product.title}</span>
+                        <span>{item.quantity}</span>
+                        <button  onClick={ () => removeFromCart(item.product.id)}>Usuń</button>
+                    </li>
+                ))}
+            </ul>
+            // <ul className="cart-items-list">
+            //   {items.map((item) => (
+            //     <li key={item.product.id} className="cart-item">
+            //       <img 
+            //         src={item.product.image} 
+            //         alt={item.product.title} 
+            //         className="cart-item-image"
+            //       />
+            //       <div className="cart-item-info">
+            //         <h3>{item.product.title}</h3>
+            //         <p>Cena: {item.product.price} zł</p>
+            //         <p>Ilość: {item.quantity}</p>
+            //       </div>
+            //       <button 
+            //         className="cart-item-remove"
+            //         onClick={() => removeFromCart(item.product.id)}
+            //       >
+            //         Usuń
+            //       </button>
+            //     </li>
+            //   ))}
+            // </ul>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
